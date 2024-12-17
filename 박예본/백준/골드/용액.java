370b5dddb8781543eb88d3a6c88ce313d54ca330
yebon <goodyebon1@gmail.com>
2024-12-18 02:30:19 +0900

    add 2467 용액 328ms

---
+++ 박예본/백준/골드/용액.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.*;
+import java.util.*;
+
+public class 용액 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] w = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int a =0; a<N;a++){
+            w[a]= Integer.parseInt(st.nextToken());
+        }
+
+        int high = N-1;
+        int low = 0;
+
+        int result_high = w[high];
+        int result_low = w[low];
+        int sum = Math.abs(result_high + result_low);
+
+        while( high > low+1 ){
+
+            if( Math.abs(w[high]) < Math.abs(w[low]) ){
+                low++;
+            }else{ //-가 더 크다면
+                high--;
+            }
+
+            if( Math.abs(w[high] + w[low]) < sum){
+                result_high = w[high];
+                result_low = w[low];
+                sum = Math.abs(w[high] + w[low]);
+            }
+
+        }
+
+        System.out.println(result_low+ " " +result_high);
+
+    }
+}
