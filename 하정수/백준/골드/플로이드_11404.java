ba763e3c0ca66bf003bad33bb39ed9908e8ebec6
HaJeongSu <htg770@gmail.com>
2025-02-26 21:49:20 +0900

    add 11404 플로이드 1336ms

--- 하정수/백준/골드/플로이드_11404.java
+++ 하정수/백준/골드/플로이드_11404.java
@@ -8,6 +8,18 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+/*
+ *  이 문제는 플로이드워셜로 풀라는 문제지만 다익스트라로 구현해보았다.
+ *  시간 복잡도를 생각해봤을 때, 플로이드워셜은 최악의 경우 N^3이고,
+ *  다익스트라는 N*MlogN 이라고 한다.
+ *  그래서 일반적인 경우는 다익스트라가 쬐끔 더 유리할 수도 있지만,
+ *  우리는 항상 최악의 경우를 생각해야 하기 때문에,
+ *  문제의 조건에서 M의 최대 개수가 100,000개 이므로 N의 최대 수인 100의 2승 10,000 보다 결국
+ *  크기 때문에 결론적으로 다익스트라는 N^3logN 이 되고, 플로이드워셜은 N^3이기 때문에
+ *  플로이드 워셜로 푸는 것이 더 적합하다고 볼 수 있다고 한다.
+ *
+ * */
+
 public class 플로이드_11404 {
     static class Node {
         int to, val;
@@ -46,7 +58,7 @@ public class 플로이드_11404 {
             int to = Integer.parseInt(st.nextToken());
             int val = Integer.parseInt(st.nextToken());
 
-            if(dist[from][to] > val){
+            if (dist[from][to] > val) {
                 dist[from][to] = val;
             }
 
